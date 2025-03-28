4155455a264122fb145b2f3ba20bf2933c75181b
Drezzlez <50885665+Drezzlez@users.noreply.github.com>
2025-03-28 01:30:12 -0400

    sync

--- src/routes/author/+page.svelte
+++ src/routes/author/+page.svelte
@@ -6,4 +6,23 @@
 
 <div class = "editorcontrols"></div>
 
-<Page><br></Page>
+<Page>
+    <input type="text" class="textinput" style="font-size:xx-large; margin-top:50px", placeholder="Title">
+    <hr>
+    <div style="background-color:rgba(0,0,0,0.5); height:200px; width:200px; float:right"></div>
+    
+    <div contenteditable="true" style="height:100px; width:100%" ></div>
+    <div contenteditable="true" style="height:100px; width:100%" ></div>
+</Page>
+
+<style>
+    .textinput{
+        background-color: rgba(240, 248, 255, 0);
+        border-style:groove;
+    }
+    .title{
+            font-size: xx-large;
+            text-wrap: balance;
+            margin-top: 50px;
+        }
+</style>
